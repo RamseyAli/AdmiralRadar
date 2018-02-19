dcac21c382f1acca313d393f1fc02e74ff325d65
octosquidport <deluns@gmail.com>
2018-02-18 22:26:41 -0500

    added a few functions and edited client

--- Workspace/AdmiralRadarServer/AdmRadarClient.java
+++ Workspace/AdmiralRadarServer/AdmRadarClient.java
@@ -1,14 +1,42 @@
 import java.io.*;
 import java.net.*;
 
-public class AdmRadarClient
-{
-	public static void main(String[] args) throws IOException
-	{
+public class AdmRadarClient {
+	private Socket primeSocket;
+	private PrintWriter outStream;
+	private BufferedReader inStream;
+	
+	private void connect_Server(String hostName, int portNumber) throws Exception {
+		primeSocket = new Socket(hostName, portNumber);
+		
+		outStream = new PrintWriter(primeSocket.getOutputStream(), true);
+		inStream = new BufferedReader(new InputStreamReader(primeSocket.getInputStream()));
+	}
+	
+	public void login() {} // TODO: Database stuff
 
-		if (args.length != 2)
-		{
-			System.err.println("Usage: java AdmRadarClient <host name> <port number>");
+	public void sendCommands() {
+
+	}
+	
+	public void sendMessages(String msg) {
+		outStream.println(msg);
+	}
+	public void getMessages() throws IOException {
+		inStream.readLine();
+	}
+	public void getShipObject(){
+		
+	}
+	static int callClientTypeGUI(){
+		return 0;
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		if (args.length != 2) {
+			System.err.println(
+				"Usage: java AdmRadarClient <host name> <port number>");
 			System.exit(1);
 		}
 
@@ -28,13 +56,13 @@ public class AdmRadarClient
 			String fromUser;
 			
 			Maps map1 = new Maps();
-			map1 = (Maps) is.readObject();
+			//map1 = (Maps) is.readObject();
 			
 			map1.printAsteroids();
 			
 			fromServer = in.readLine();
 			System.out.println("Server: " + fromServer);
-			
+
 			fromUser = stdIn.readLine();
 			if (fromUser != null)
 			{
@@ -42,15 +70,14 @@ public class AdmRadarClient
 				out.println(fromUser);
 			}
 	    
-			while (true)
+			while ((fromServer = in.readLine()) != null)
 			{
-				fromServer = in.readLine();
 				System.out.println("Server: " + fromServer);
 				if (fromServer.equals("Bye."))
 					break;
 				fromServer = in.readLine();
 				System.out.println("Server: " + fromServer);
-				
+
 				fromUser = stdIn.readLine();
 				if (fromUser != null)
 				{
@@ -61,12 +88,8 @@ public class AdmRadarClient
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostName);
 			System.exit(1);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			System.err.println("Couldn't get I/O for the connection to " + hostName);
-			System.exit(1);
-		} catch (ClassNotFoundException e) {
-			System.err.println("Other Exceptions");
-			System.out.println(e.getMessage());
 			System.exit(1);
 		}
 	}
