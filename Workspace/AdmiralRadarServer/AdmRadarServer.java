664cb36e10db7ac93d1e9f4fafc8e30dc48ad89b
Ramsey Ali <ali59@purdue.edu>
2018-02-20 14:35:38 -0500

    Database Integratioon
    *Server can now talk to database
    *Login functionality added

--- Workspace/AdmiralRadarServer/AdmRadarServer.java
+++ Workspace/AdmiralRadarServer/AdmRadarServer.java
@@ -1,5 +1,7 @@
 import java.net.*;
 import java.io.*;
+import java.sql.*;
+import java.util.*;
 
 public class AdmRadarServer
 {
@@ -47,5 +49,112 @@ public class AdmRadarServer
 			System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
 			System.out.println(e.getMessage());
 		}
+
+
 	}
+
+	public static class dbQuery {
+		Connection conn = null;
+		Statement statement = null;
+		ResultSet rs = null;
+
+		public dbQuery(Connection conn, Statement statement, ResultSet rs) {
+			this.conn = conn;
+			this.rs = rs;
+		}
+
+		public boolean close() {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+			return true;
+		}
+	}
+
+
+	public static dbQuery query(String query) {
+
+		Connection conn = null;
+
+		try {
+
+			String url = "jdbc:mysql://radar.c87i64zdxk4i.us-east-2.rds.amazonaws.com:3306/AdmiralRadar";
+			Properties info = new Properties();
+			info.put("user", "admin");
+			info.put("password", "password1234");
+
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			conn = DriverManager.getConnection(url, info);
+
+		} catch (Exception ex) {
+			System.out.println("An error occurred while connecting MySQL databse");
+			ex.printStackTrace();
+			return null;
+		}
+
+		if (conn != null) {
+			System.out.println("Successfully connected to MySQL database");
+		} else {
+			System.out.println("Could not connect to MySQL database");
+			return null;
+		}
+
+		//Connection established, run query...
+		Statement statement = null;
+
+		try {
+
+			statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(query);
+			dbQuery obj = new dbQuery(conn, statement, rs);
+			return obj;
+
+		} catch (Exception e) {
+			System.out.println("Could not execute query on MySQL database");
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/*
+	0 - Success
+	1 - Invalid Username
+	2 - Invalid Password
+	*/
+	public static int login(String user, String pw) {
+
+		dbQuery DBobj = query("SELECT USERNAME, PASSWORD FROM USER");
+
+		try {
+			while (DBobj.rs.next()) {
+				if (user.equals(DBobj.rs.getString("USERNAME"))) {
+					if (user.equals(DBobj.rs.getString("PASSWORD"))) {
+						return 0;
+					} else {
+						return 2;
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("There's an issue retrieving info. from query results");
+		} finally {
+			DBobj.close();
+			return 1;
+		}
+
+	}
+
+
 }
