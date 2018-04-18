033e9316f2cb5250a8c7a3c18be89e41faed0b89
Sam Buck <sam@sacunderwatertechnologies.com>
2018-04-18 17:20:31 -0400

    Fix Sound Code

--- Workspace/AdmiralRadarClient/src/audio/SoundManager.java
+++ Workspace/AdmiralRadarClient/src/audio/SoundManager.java
@@ -7,39 +7,39 @@ import game.Role;
 
 public class SoundManager {
 
+	
+	public static final int MSG_SND_SOUND = 1;
+	public static final int MSG_REC_SOUND = 2;
+	public static final int MIN_PUT_SOUND = 3;
+	public static final int MIN_EXP_SOUND = 4;
+	public static final int MIS_LNH_SOUND = 5;
+	public static final int MIS_EXP_SOUND = 6;
+	public static final int SENSORS_SOUND = 7;
+	public static final int MSE_HVR_SOUND = 8;
+	public static final int MSE_CLK_SOUND = 9;
+	
 	private static Map<Role, MyPlayer> 		roleMusic;
-	private static Map<Integer, MyPlayer> 	soundEffects;
-	private static Map<Integer, MyPlayer>	soundtrack;
+	private static MyPlayer[] 	soundEffects = new MyPlayer[10];
 	
 	static{
 		// Map Role to Associated Audio File
 		roleMusic = new HashMap<Role, MyPlayer>();
-		roleMusic.put( Role.CAPTAIN , new MyPlayer(Role.CAPTAIN.name()) );
-		roleMusic.get( Role.CAPTAIN ).makeLoopable();
-		
-		roleMusic.put( Role.FIRST , new MyPlayer(Role.FIRST.name()) );
-		roleMusic.get( Role.FIRST ).makeLoopable();
-		
-		roleMusic.put( Role.ENGINE , new MyPlayer(Role.ENGINE.name()) );
-		roleMusic.get( Role.ENGINE ).makeLoopable();
-		
-		roleMusic.put( Role.NETWORK , new MyPlayer(Role.NETWORK.name()) );
-		roleMusic.get( Role.NETWORK ).makeLoopable();
-		
-		roleMusic.put( Role.RADIO , new MyPlayer(Role.RADIO.name()) );
-		roleMusic.get( Role.RADIO ).makeLoopable();
+		generatePlayer(Role.CAPTAIN);
+		generatePlayer(Role.FIRST);
+		generatePlayer(Role.ENGINE);
+		generatePlayer(Role.NETWORK);
+		generatePlayer(Role.RADIO);
 		
 		// Map integer value to every Sound Effect
-		soundEffects = new HashMap<Integer, MyPlayer>();
-		soundEffects.put(0, new MyPlayer("MessageSend"));
-		soundEffects.put(1, new MyPlayer("MessageRecieved"));
-		soundEffects.put(2, new MyPlayer("MinePlace"));
-		soundEffects.put(3, new MyPlayer("MineExplosion"));
-		soundEffects.put(4, new MyPlayer("MissileLaunch"));
-		soundEffects.put(5, new MyPlayer("MissileExplosion"));
-		soundEffects.put(6, new MyPlayer("sensorySounds"));					// Drone and Sonar
-		soundEffects.put(7, new MyPlayer("MouseHover"));
-		soundEffects.put(8, new MyPlayer("MineClick"));			
+		soundEffects[0] = new MyPlayer("MessageSend");
+		soundEffects[1] = new MyPlayer("MessageRecieved");
+		soundEffects[2] = new MyPlayer("MinePlace");
+		soundEffects[3] = new MyPlayer("MineExplosion");
+		soundEffects[4] = new MyPlayer("MissileLaunch");
+		soundEffects[5] = new MyPlayer("MissileExplosion");
+		soundEffects[6] = new MyPlayer("sensorySounds");					// Drone and Sonar
+		soundEffects[7] = new MyPlayer("MouseHover");
+		soundEffects[8] = new MyPlayer("MineClick");			
 		// Add Boost, Spacewalk 15,16
 	}
 	
@@ -48,6 +48,13 @@ public class SoundManager {
 	}
 	
 	
+	private static void generatePlayer(Role r) {
+		roleMusic.put( r , new MyPlayer(r.name()) );
+		roleMusic.get( r ).makeLoopable();
+		
+	}
+
+
 	public static void stopRoleTrack(Role r) {
 		try {
 			roleMusic.get( r ).stop();
@@ -57,7 +64,7 @@ public class SoundManager {
 	}
 	
 	public static void playSoundEffect(int t) {
-		soundEffects.get(t).play();
+		soundEffects[t].play();
 	}
 
 }
