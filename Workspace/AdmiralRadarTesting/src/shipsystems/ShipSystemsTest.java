f5dfaf5b901e09bcfa79c7c7785805db9e888f7d
octosquidport <deluns@gmail.com>
2018-04-19 14:54:55 -0400

    continued Radar tests

--- Workspace/AdmiralRadarTesting/src/shipsystems/ShipSystemsTest.java
+++ Workspace/AdmiralRadarTesting/src/shipsystems/ShipSystemsTest.java
@@ -40,43 +40,7 @@ public class ShipSystemsTest {
 		}
 		
 		System.out.println();
-	}
-	private static void printTrueInfo(int index) {
-		System.out.print("Ship #");
-		System.out.println(index);
-		System.out.print("    X: ");
-		System.out.println(ships.get(index).getPosition().getX());
-		System.out.print("    Y: ");
-		System.out.println(ships.get(index).getPosition().getY());
-		System.out.print("    Sector: ");
-		System.out.println(ships.get(index).getSector(SEG, SEC));
-	}
-	private static void printRadar(int index) {
-		int radarInfo[];
-		
-		radarInfo = ships.get(index).randomRadar(SEG, SEC);
-		
-		System.out.println("Radar Gives");
-		
-		System.out.print("    X: ");
-		if (radarInfo[0] > -1)
-			System.out.println(radarInfo[0]);
-		else
-			System.out.println("Ommitted");
-		
-		System.out.print("    Y: ");
-		if (radarInfo[1] > -1)
-			System.out.println(radarInfo[1]);
-		else
-			System.out.println("Ommitted");
-		
-		System.out.print("    Sector: ");
-		if (radarInfo[2] > -1)
-			System.out.println(radarInfo[2]);
-		else
-			System.out.println("Ommitted");
-	}
-	
+	}	
 	private static void testDrone() {
 		final int lengthX = 20, lengthY = 30;
 		final int TEST_SIZE = lengthX * lengthY;
@@ -103,12 +67,68 @@ public class ShipSystemsTest {
 		}
 	}
 	
+	private static void printTrueInfo(int index) {
+		System.out.print("Ship #");
+		System.out.println(index);
+		System.out.print("    X: ");
+		System.out.println(ships.get(index).getPosition().getX());
+		System.out.print("    Y: ");
+		System.out.println(ships.get(index).getPosition().getY());
+		System.out.print("    Sector: ");
+		System.out.println(ships.get(index).getSector(SEG, SEC));
+	}
+	private static boolean matchRadar(int index) {
+		int i, ommissions = 0, truths = 0, lies = 0;
+		int radarInfo[];
+		int trueInfo[] = new int[3];
+		
+		trueInfo[0] = ships.get(index).getPosition().getX();
+		trueInfo[1] = ships.get(index).getPosition().getY();
+		trueInfo[2] = ships.get(index).getSector(SEG, SEC);
+		
+		radarInfo = ships.get(index).randomRadar(SEG, SEC);
+		
+		System.out.println("Radar Gives");
+		
+		System.out.print("    X: ");
+		if (radarInfo[0] > -1)
+			System.out.println(radarInfo[0]);
+		else
+			System.out.println("Ommitted");
+		
+		System.out.print("    Y: ");
+		if (radarInfo[1] > -1)
+			System.out.println(radarInfo[1]);
+		else
+			System.out.println("Ommitted");
+		
+		System.out.print("    Sector: ");
+		if (radarInfo[2] > -1)
+			System.out.println(radarInfo[2]);
+		else
+			System.out.println("Ommitted");
+		
+		for (i = 0; i < 3; ++i)
+		{
+			if (radarInfo[i] < 0) 
+				++ommissions;
+			else if (radarInfo[i] == trueInfo[i])
+				++truths;
+			else
+				++lies;
+		}
+		
+		if (ommissions == 1 && truths == 1 && lies == 1)
+			return true;
+		
+		return false;
+	}
 	private static void testRadar() {
 		final int lengthX = 15, lengthY = 15;
 		final int TEST_SIZE = lengthX * lengthY;
 		final int startX = 0, startY = 0;
 		
-		int i, j;
+		int i, j, totalSuccess = 0;
 		
 		for (i = 0; i < TEST_SIZE; ++i)
 		{
@@ -119,20 +139,32 @@ public class ShipSystemsTest {
 		for (i = 0; i < TEST_SIZE; ++i)
 		{
 			printTrueInfo(i);
-			printRadar(i);
+			if (matchRadar(i))
+			{
+				++totalSuccess;
+				System.out.println("Test Passed");
+			}
+			else
+				System.out.println("Test Failed");
+			
 			System.out.println();
 		}
 		
-		
-		
+		System.out.print("Passed ");
+		System.out.print(totalSuccess);
+		System.out.print(" out of ");
+		System.out.println(TEST_SIZE);
 	}
 	
 	public static void main(String[] args) throws Exception {
 		PrintStream stdout = System.out;
+		PrintStream out;
 		
+		out = new PrintStream(new FileOutputStream("RadarTest.txt"));
+		System.setOut(out);
 		testRadar();
 		
-		PrintStream out = new PrintStream(new FileOutputStream("DroneTest.txt"));
+		out = new PrintStream(new FileOutputStream("DroneTest.txt"));
 		System.setOut(out);
 		
 		testDrone();
